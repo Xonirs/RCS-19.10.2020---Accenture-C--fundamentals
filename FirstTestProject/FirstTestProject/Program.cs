895976c6cb05287b0ee253c4c609dc8326d3ede5
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-10-19 21:30:17 +0300

    test commit

---
+++ FirstTestProject/FirstTestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today was... quite sad for me.");
+
+            Console.WriteLine("Hey cool, this is my second line!");
+
+            Console.WriteLine("Hey cool, this is my third line using copy!");
+
+            Console.WriteLine("Hey cool, this is my fourth line using copy with CTRL+C and CTRL+V!");
+
+            Console.WriteLine("Today ending was... quite fun for me to meet so many cool students!");
+
+            Console.WriteLine("Hey cool, this is my END line!");
+            Console.WriteLine("Hey cool, this is my END line nr.2!");
+            Console.WriteLine("Hey cool, this is my END line nr.3!");
+            Console.WriteLine("Hey cool, this is my END line nr.4 with EDIT->DUPLICATE or CTRL+D!");
+        }
+    }
+}
---
+++ FirstWebTestApp/FirstWebTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstWebTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
