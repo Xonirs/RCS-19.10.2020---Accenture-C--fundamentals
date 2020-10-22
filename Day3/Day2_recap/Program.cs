5c48446a285d6a68a397327fd3febb47a2fed3f5
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-10-22 19:39:14 +0300

    day3 initial

---
+++ Day3/Day2_recap/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Day2_recap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This text will be printed");
+            Console.WriteLine();
+
+            Console.WriteLine("Simple variable stuff:");
+            string name = "John";
+            int age = 54;
+            double salary = 450.00;
+            bool didIhaveBreakfast = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(salary);
+            Console.WriteLine(didIhaveBreakfast);
+            Console.WriteLine();
+
+            //string name = "Dave"; //this won't work, can't have duplicate variable name
+            name = "Dave";
+            Console.WriteLine(name);
+
+            bool itWillSnowTomorrow;
+            //....
+            Console.WriteLine("Weather predictor says it will snow tomorrow");
+            itWillSnowTomorrow = true;
+
+            int number = 37;
+            int otherNumber = 12;
+            int sum = number + otherNumber;
+            Console.WriteLine(sum);
+
+            Console.WriteLine("How to read from console:");
+            Console.WriteLine("Please enter your name:");
+            string userNameInput = Console.ReadLine(); //from CONSOLE we can read ONLY text values
+
+            Console.WriteLine("Please enter your surname:");
+            string userSurnameInput = Console.ReadLine();
+
+            string userIdentity = userNameInput + " " + userSurnameInput;
+            Console.WriteLine(userIdentity);
+            Console.WriteLine(userNameInput + " " + userSurnameInput);
+            Console.WriteLine();
+            
+            string userIdentity2 = $"{userNameInput} {userSurnameInput}";
+            Console.WriteLine(userIdentity2);
+            Console.WriteLine($"{userNameInput} {userSurnameInput}");
+
+            string userIdentity3 = string.Format("{0} {1}", userNameInput, userSurnameInput);
+            Console.WriteLine(userIdentity3);
+            Console.WriteLine(string.Format("{0} {1}. Color: {2}. {0} {1}. Age: {3}", userNameInput, userSurnameInput, "red", 24));
+
+
+            Console.WriteLine("END OF CODE");
+        }
+    }
+}
+
---
+++ Day3/Day3_string/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day3_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmsrklhpsfiodsiodgjskldgjskldsdklghsdklghsnopqrstuvwqyz";
+
+            //Substring - to get a part of a string
+
+            //0 - the place to start the part of the text
+            //5 - how many letters to take
+            //0 - starting position
+            //5 - text length
+            string first5Letters = alphabet.Substring(0, 5);
+            Console.WriteLine($"First 5 letters of the alphabet are {first5Letters}");
+
+            string from3For7letters = alphabet.Substring(2, 7);
+            Console.WriteLine($"7 letters from the 3rd position of the alphabet are {from3For7letters}");
+
+
+            //9 - starting position. the part of the text will be taken from position with index 9
+            //if the length is not provided, then from the starting position the rest of the text is taken
+            string from10TillEnd = alphabet.Substring(9);
+            Console.WriteLine($"From the 10th position rest of the alphabet is {from10TillEnd}");
+            Console.WriteLine($"From the 10th position rest of the alphabet is {alphabet.Substring(9)}");
+
+
+            //Length
+            int alphabetLength = alphabet.Length;
+            Console.WriteLine($"The length of the alphabet is {alphabetLength}");
+
+            string lastAlphabetLetter = alphabet.Substring(alphabet.Length - 1);
+            Console.WriteLine($"The last letter of the alphabet is {lastAlphabetLetter}");
+        }
+    }
+}
