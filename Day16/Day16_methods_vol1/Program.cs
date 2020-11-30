b64a92eb5d274b685552a95a748e91273d98adee
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-11-30 19:06:59 +0200

    day 16 methods, without methods

---
+++ Day16/Day16_methods_vol1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day16_methods_vol1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+//how does data move between Main method and our own custom created methods
+//
+//difference between returning values and passing values as parameters
+//
+//methods with parameters. gathering data from user. if user hasn't given any data.
+//examples for methods with parameters + Console.ReadLine
+//
+//default values, when user doesn't input 1 value.
+//when user inputs empty string
+//
+//getting info in 1 method and printing in another
+//
+//options when generating lists
+//
+//
---
+++ Day16/Day16_withoutMethods/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Day16_withoutMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] {  80, 10, 40, 50, 60, 80, 111, 233, -5, -12 };
+            Console.WriteLine("Here is the numbers array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Let's change some values");
+            numbers[0] = 999;
+            numbers[4] = 88;
+            numbers[7] = 777;
+            Console.WriteLine("Array values have been changed");
+
+            Console.WriteLine("Here is the numbers array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Let's change some values, first 3 values should be negative");
+            numbers[0] = -55;
+            numbers[1] = -33;
+            numbers[2] = -44;
+            Console.WriteLine("Array values have been changed");
+
+            Console.WriteLine("Here is the numbers array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Let's allow the user to input array values 1,4,5,10.");
+
+            Console.WriteLine("Please input the new value for array element nr.1");
+            string newValue1input = Console.ReadLine();
+            int newValue1 = int.Parse(newValue1input);
+            numbers[0] = newValue1; //GetNewArrayElement();
+
+            Console.WriteLine("Please input the new value for array element nr.4");
+            string newValue4input = Console.ReadLine();
+            int newValue4 = int.Parse(newValue4input);
+            numbers[3] = newValue4;
+
+            Console.WriteLine("Please input the new value for array element nr.5");
+            string newValue5input = Console.ReadLine();
+            int newValue5 = int.Parse(newValue5input);
+            numbers[4] = newValue5;
+
+            Console.WriteLine("Please input the new value for array element nr.10");
+            string newValue10input = Console.ReadLine();
+            int newValue10 = int.Parse(newValue10input);
+            numbers[9] = newValue10;
+
+            Console.WriteLine("Here is the numbers array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
